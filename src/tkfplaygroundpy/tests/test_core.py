2680ed5948d8cb4828c438ce3c386c22c8770d06
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-20 20:30:04 -0700

    Add test_version

--- src/tkfplaygroundpy/tests/test_core.py
+++ src/tkfplaygroundpy/tests/test_core.py
@@ -1,2 +1,11 @@
+from pkg_resources import get_distribution
+
+from .. import __version__
+
+
 def test_always_pass():
     pass
+
+
+def test_version():
+    assert get_distribution("tkfplaygroundpy").version == __version__
